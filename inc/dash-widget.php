ed098ae3afd49afd1e4bec0eb91a5c98f30ac29e
Dan Beil <dan@alleyinteractive.com>
2015-01-18 15:52:08 -0600

    working dash widget saving

--- inc/dash-widget.php
+++ inc/dash-widget.php
@@ -1,17 +1,17 @@
 <?php
-
-echo 'dash widget';
-
 if ( ! class_exists( 'DB_Image_Replace_Dash_Widget' ) ) {
 
 	/**
-	 * undocumented class
+	 * The Image Replace dashboard widget
 	 *
-	 * @package default
-	 * @author
+	 * @package Image Replace
+	 * @author Dan Beil
 	 **/
 	class DB_Image_Replace_Dash_Widget {
 
+		/**
+		 * Array keys correspond to image directories, i.e. imgs/futurama
+		 */
 		public $image_dirs = array(
 			'futurama' => 'Futurama',
 			'star-trek' => 'Star Trek',
@@ -28,37 +28,43 @@ if ( ! class_exists( 'DB_Image_Replace_Dash_Widget' ) ) {
 				array( $this, 'render_dashboard_widget' ),
 				array( $this, 'configure_dashboard_widget' )
 			);
-			echo 'post<pre>';
-			print_r($_GET);
-			echo '</pre>';
 		}
 
 		public function render_dashboard_widget() {
 			$db_ir_options = get_option( 'db_ir_options' );
-			echo 'from options<pre>';
-			print_r($db_ir_options );
-			echo '</pre>';
-			echo 'working';
-			printf( __( 'Developer Fuel must be configured to work properly. Please <a href="%s">click here</a> to configure it now!', 'dbimagereplace' ), add_query_arg( array( 'edit' => 'dbimagereplace' ) ) );
+			$edit_text = sprintf( __( 'Update your Image Replace options <a href="%s">HERE</a>', 'dbimagereplace' ), add_query_arg( array( 'edit' => 'dbimagereplace' ) ) );
+
+			if ( ! empty( $db_ir_options ) ) {
+				echo '<h2>' . esc_html__( 'Your are currently using:' ) . '</h2>';
+				echo '<ul>';
+				foreach ( $db_ir_options as $key => $value) {
+					echo '<li>' . esc_html( $value ) . '</li>';
+				}
+				echo '</ul>';
+			}
+			echo $edit_text;
 		}
 
 		public function configure_dashboard_widget() {
-			echo 'dfhadjshfjshfksajdhfjsdkahfljshafjhsdajfhasljfhconfig';
+			$db_ir_options = get_option( 'db_ir_options' );
+			$array_for_saving = array(); // needed for saving
+			echo '<h2>' . esc_html__( 'Image Replace Options:' ) . '</h2>';
 
-			$array_for_saving = array();
-
+			// Options output
 			foreach ( $this->image_dirs as $key => $value) {
-				echo '<input type="checkbox" name="' . esc_attr( $key ) . '" value="' . esc_attr( $key ) . '">' . esc_html( $value, 'dbimagereplace' ) . '</br>';
+				$checked = array_key_exists( $key, $db_ir_options) ? 'checked' : '';
+				echo '<input type="checkbox" ' . $checked . ' name="' . esc_attr( $key ) . '" value="' . esc_attr( $key ) . '">' . esc_html( $value, 'dbimagereplace' ) . '</br>';
 			}
-		    echo '<input type="hidden" name="dbimagereplace_save" value="true" />';
+			echo '<input type="hidden" name="dbimagereplace_save" value="true" />';
+			echo '<hr /><br />';
 
-		    if ( isset( $_POST['dbimagereplace_save'] ) ) {
-		    	foreach ( $this->image_dirs as $key => $value) {
-		    		if ( array_key_exists( $key, $_POST ) )
-		    			$array_for_saving[ $key ] = $value;
-		    	}
-		    }
-
+			// Creating array for saving so we dont save all $_POST data here
+			if ( isset( $_POST['dbimagereplace_save'] ) ) {
+				foreach ( $this->image_dirs as $key => $value) {
+					if ( array_key_exists( $key, $_POST ) )
+						$array_for_saving[ $key ] = $value;
+				}
+			}
 			update_option( 'db_ir_options', $array_for_saving );
 
 		}
