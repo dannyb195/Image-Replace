53884690acc118f4f38e953dfc636e99d3e1ce34
Dan Beil <dan@alleyinteractive.com>
2015-01-17 20:54:43 -0600

    removing unused vars, correct .gitignore

--- image-replace.php
+++ image-replace.php
@@ -58,9 +58,6 @@ if ( ! class_exists( 'DB_Image_Replace' ) ) {
 
 		public function image_src_filter( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
 
-			// echo '<pre>';
-			// print_r($img_id_hash);
-			// echo '</pre>';
 			// dealing with different $size array on single.php
 			if ( is_array( $size ) && isset( $size[0] ) ) {
 				$target_w = $size[0];
@@ -73,8 +70,6 @@ if ( ! class_exists( 'DB_Image_Replace' ) ) {
 			$rand = rand( 0, $count - 1 );
 			$img_id_hash = hash( 'md5', basename( $this->img_files[ $rand ] ) );
 
-
-
 			$image = DB_IMAGE_REPLACE_PATH . 'imgs/futurama/' . basename( $this->img_files[ $rand ] ); // the image to crop
 			$dest_image = 'imgs/temp/' . $img_id_hash . '-' . $target_w . 'x' . $target_h . '.jpg'; // make sure the directory is writeable
 			$img = imagecreatetruecolor( intval( $target_w ), intval( $target_h ) );
@@ -83,11 +78,9 @@ if ( ! class_exists( 'DB_Image_Replace' ) ) {
 			$target_x_start = ( $original_size[0] / 2 ) - ( $target_w / 2 );
 			$target_y_start = ( $original_size[1] / 2 ) - ( $target_h / 2 );
 
-			fopen( DB_IMAGE_RELPACE_LOCAL_PATH . 'imgs/temp/cropped_whatever.jpg', 'w' );
+			fopen( DB_IMAGE_RELPACE_LOCAL_PATH . 'imgs/temp/' . $img_id_hash . '-' . $target_w . 'x' . $target_h . '.jpg', 'w' );
 			imagecopy( $img, $org_img, 0, 0, $target_x_start, $target_y_start, intval( $target_w ), intval( $target_h ) );
 			imagejpeg( $img, DB_IMAGE_RELPACE_LOCAL_PATH . $dest_image, 90 );
-
-			$img_info = wp_get_attachment_image_src( $post_thumbnail_id, $size );
 
 			$html = '<img src="' . esc_url( DB_IMAGE_REPLACE_PATH . 'imgs/temp/' . basename( $dest_image ) ) . '" width="' . intval( $target_w ) . '" height="' . intval( $target_h ) . '" />';
 			return $html;
