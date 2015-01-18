eda1152933b7cf758fedb6fe2d0ab664674f8b2d
Dan Beil <dan@alleyinteractive.com>
2015-01-17 20:09:07 -0600

    inital cropping work

--- image-replace.php
+++ image-replace.php
@@ -21,6 +21,8 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 define( 'DB_IMAGE_REPLACE_PATH', plugins_url( '/', __FILE__ ) );
 define( 'DB_IMAGE_REPLACE_IMAGE_DIR_PATH', dirname( realpath( __FILE__ ) ).'/imgs/futurama/' );
+define( 'DB_IMAGE_RELPACE_LOCAL_PATH', plugin_dir_path( __FILE__ ) );
+echo DB_IMAGE_RELPACE_LOCAL_PATH;
 
 if ( ! class_exists( 'DB_Image_Replace' ) ) {
 
@@ -46,17 +48,39 @@ if ( ! class_exists( 'DB_Image_Replace' ) ) {
 		}
 
 		public function image_arrays() {
-	       foreach(glob( DB_IMAGE_REPLACE_IMAGE_DIR_PATH .'*') as $filename){
-			    $this->img_files[] = basename( $filename );
+			foreach ( glob( DB_IMAGE_REPLACE_IMAGE_DIR_PATH .'*') as $filename){
+				$this->img_files[] = basename( $filename );
 			}
 			shuffle( $this->img_files );
+
+
 		}
 
 		public function image_src_filter( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
-			$img_info = wp_get_attachment_image_src( $post_thumbnail_id, $size );
+			// dealing with different $size array on single.php
+			if ( is_array( $size ) && isset( $size[0] ) ) {
+				$org_w = $size[0];
+				$org_h = $size[1];
+			} else {
+				$org_w = $this->img_sizes[ $size ]['width'];
+				$org_h = $this->img_sizes[ $size ]['height'];
+			}
+
 			$count = count( $this->img_files );
 			$rand = rand( 0, $count - 1 );
-			$html = '<img src="' . esc_url( DB_IMAGE_REPLACE_PATH . 'imgs/futurama/' . basename( $this->img_files[ $rand ] ) ) . '" width="' . intval( $img_info[1] ) . '" height="' . intval( $img_info[2] ) . '" />';
+
+			$image = DB_IMAGE_REPLACE_PATH . 'imgs/futurama/' . basename( $this->img_files[ $rand ] ); // the image to crop
+			fopen( DB_IMAGE_RELPACE_LOCAL_PATH . 'imgs/temp/cropped_whatever.jpg', 'w' );
+			$dest_image = 'imgs/temp/cropped_whatever.jpg'; // make sure the directory is writeable
+			$img = imagecreatetruecolor( intval( $org_w ), intval( $org_h ) );
+			$org_img = imagecreatefromjpeg( $image );
+			$ims = getimagesize( $image );
+			imagecopy( $img, $org_img, 0, 0, 20, 20, intval( $org_w ), intval( $org_h ) );
+			imagejpeg( $img, DB_IMAGE_RELPACE_LOCAL_PATH . $dest_image, 90 );
+
+			$img_info = wp_get_attachment_image_src( $post_thumbnail_id, $size );
+
+			$html = '<img src="' . esc_url( DB_IMAGE_REPLACE_PATH . 'imgs/temp/' . basename( $dest_image ) ) . '" width="' . intval( $org_w ) . '" height="' . intval( $org_h ) . '" />';
 			return $html;
 		}
 
